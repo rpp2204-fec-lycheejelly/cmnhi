1c5d57e28a24fd1ad8200324aadb12e6f62987c6
Miranda Zhou <miranda@Mirandas-MacBook-Pro.local>
2022-08-18 18:24:23 -0700

    Set up test suite

--- client/src/product-overview/Product.jsx
+++ client/src/product-overview/Product.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+
+class Product extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = ({
+
+    })
+  }
+}
 export default Product
