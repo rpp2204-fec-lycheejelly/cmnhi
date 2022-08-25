e4f1c4f7fc22721a063a4e3d05bddc3486b6c670
Ethan Ayaay <ayaayethan@gmail.com>
2022-08-24 20:17:17 -0700

    Minor changes

--- client/src/product-overview/StyleSelect.jsx
+++ client/src/product-overview/StyleSelect.jsx
@@ -5,7 +5,7 @@ let StyleSelect = (props) => {
     <h3>Style Selector</h3>
     <p>Selected Style</p>
     <div>Styles</div>
-    <button>Add To Outfit</button>
+    <button>&#9734;</button>
   </div>
 }
 
