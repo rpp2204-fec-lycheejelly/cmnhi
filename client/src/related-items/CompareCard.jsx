0ff663a953333d0365dfdf5f9d18918a83798496
Miranda Zhou <miranda@Mirandas-MacBook-Pro.local>
2022-08-26 22:52:13 -0700

    Finished major related product info

--- client/src/related-items/CompareCard.jsx
+++ client/src/related-items/CompareCard.jsx
@@ -5,18 +5,52 @@ class CompareCard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      product: ''
+      products: []
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.related.length !== prevProps.related.length) {
+      this.setState({
+        products: this.props.related
+      })
     }
   }
 
   render () {
-    return (
-      <div id="related">
-        <p>Category</p>
-        <p>Product Name</p>
-        <p>*****</p>
-      </div>
-    )
+    console.log(this.state.products);
+    if (this.state.products) {
+      return (
+        <div id="related">
+        {this.state.products.map((item, id) => {
+          let ratings = item.ratings;
+          let total = 0;
+          let count = 0;
+          for (var key in ratings) {
+            total += ratings[key] * key;
+            count += Number(ratings[key]);
+          }
+          console.log(total);
+          console.log(count);
+          return (
+            <div id="items" key={id}>
+              <p>{item.category}</p>
+              <p><strong>{item.name}</strong></p>
+              <p>{item.default_price}</p>
+              <p>Rating: {total / count}</p>
+            </div>
+          )
+        })}
+        </div>
+      )
+    } else {
+      return (
+        <div>
+
+        </div>
+      )
+    }
+
   }
 }
 export default CompareCard
