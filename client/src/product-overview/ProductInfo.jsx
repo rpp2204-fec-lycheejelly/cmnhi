331f288ad8ecdc3070e035d7d8d73278f56924c8
Ethan Ayaay <ayaayethan@gmail.com>
2022-09-03 21:08:54 -0700

    Refactors star component to be more reusable (hopefully)

--- client/src/product-overview/ProductInfo.jsx
+++ client/src/product-overview/ProductInfo.jsx
@@ -3,11 +3,14 @@ import Stars from './Stars.jsx';
 
 let ProductInfo = ({product, reviews, style}) => {
   return <div>
-          <Stars reviews={reviews}/>
-          <p className="product-category">{product.category}</p>
-          <h4 className="product-title">{product.name}</h4>
-          <span className={style.sale_price ? 'discounted price' : 'product-price'}>${style.original_price} </span>
-          {style.sale_price && <span className="sale-price product-price">${style.sale_price}</span>}
+          <div className="rating-container">
+              <Stars reviews={reviews}/>
+              <a className='review-link'>Read All Reviews</a>
+          </div>
+            <p className="product-category">{product.category}</p>
+            <h4 className="product-title">{product.name}</h4>
+            <span className={style.sale_price ? 'discounted price' : 'product-price'}>${style.original_price} </span>
+            {style.sale_price && <span className="sale-price product-price">${style.sale_price}</span>}
         </div>
 }
 
--- client/src/product-overview/Stars.jsx
+++ client/src/product-overview/Stars.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import calculateAverage from './lib/calculateAvg.js';
 import starImage from '../assets/star.png';
 
+import fullStar from '../assets/full-star.png';
+import threeQuarterStar from '../assets/three-quarter-star.png';
+import halfStar from '../assets/half-star.png';
+import quarterStar from '../assets/quarter-star.png';
+import emptyStar from '../assets/empty-star.png';
+
 let Stars = ({reviews}) => {
   let avg = calculateAverage(reviews).scores;
   let total = calculateAverage(reviews).totalResponses;
@@ -9,13 +15,18 @@ let Stars = ({reviews}) => {
     return (
       <div className="stars">
         {avg.map((star, i) => {
-          return (<div className="star-container" key={i}>
-                    <div className="star-fill" style={{"width" : `${parseInt(star*31)}px`}}>
-                      <img className="star-outline" src={starImage} alt="star alt"></img>
-                    </div>
-                  </div>)
+          if (star === 1) {
+            return <img src={fullStar} alt='full-star' className='rating-star' key={i}></img>
+          } else if (star === 0.75) {
+            return <img src={threeQuarterStar} alt='three-quart-star' className='rating-star' key={i}></img>
+          } else if (star === 0.5) {
+            return <img src={halfStar} alt='half-star' className='rating-star' key={i}></img>
+          } else if (star === 0.25) {
+            return <img src={quarterStar} alt='quarter-star' className='rating-star' key={i}></img>
+          } else {
+            return <img src={emptyStar} alt='empty-star' className='rating-star' key={i}></img>
+          }
         })}
-        <a className='review-link'>Read All {total} Reviews</a>
       </div>
     )
 }
