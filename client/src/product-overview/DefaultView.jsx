75b3330b4572f70320691aed3192d4f0ff4faa10
Ethan Ayaay <ayaayethan@gmail.com>
2022-09-06 19:21:01 -0700

    Commit before merging new pull request

--- client/src/product-overview/DefaultView.jsx
+++ client/src/product-overview/DefaultView.jsx
@@ -5,9 +5,9 @@ import rightArrow from '../assets/arrow-right.png';
 let DefaultView = ({photos, index, onRightArrowClick, onLeftArrowClick}) => {
   return (
     <div className="image-default-view">
-      {index !== 0 && <img src={leftArrow} alt="left-arrow" className="left-arrow" onClick={onLeftArrowClick}></img>}
+      {index !== 0 && <img src={leftArrow} alt="leftarrow" className="left-arrow" onClick={onLeftArrowClick}></img>}
       {!photos.length ? <></> : <img className="main-img" src={photos[index].url}></img>}
-      {index !== photos.length - 1 && <img src={rightArrow} alt="right-arrow" className="right-arrow" onClick={onRightArrowClick}></img>}
+      {index !== photos.length - 1 && <img src={rightArrow} alt="rightarrow" className="right-arrow" onClick={onRightArrowClick}></img>}
     </div>
   )
 };
